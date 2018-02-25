6c8ad7fd4de70e1d1278f36679592dae060c3b78
sunsteam <sunsteam@163.com>
2018-02-25 17:56:33 +0800

    增加 macOS 下使用的命令文件 更新编译环境

--- app/src/main/autoGenDp.bat
+++ app/src/main/autoGenDp.bat
@@ -1,2 +1,2 @@
-java -jar %~dp0\autoGenDp.jar 360 320 360 384 392 400 411 533 590 -a -v19
+java -jar %~dp0\autoGenDp.jar 368 320 360 384 392 400 411 533 590 -a -v19
 pause
---
+++ autoGenDp.sh
@@ -0,0 +1,3 @@
+JARFILE=`find . -name "autoGenDp.jar"`
+cd $(dirname $JARFILE)
+java -jar autoGenDp.jar 368 320 360 384 392 400 411 533 590 -a -v19
